3a84c8b69be30b19d52ad2f5cb4deaa20de372b8
pygman <pygman@aliyun.com>
2017-07-31 14:52:02 +0800

    ejs server

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
+var http = require('http');
+var path = require('path');
 var ejs = require('ejs');
+var url = require('url');
 var fs = require('fs');
 
-index_ejs_str = fs.readFileSync(__dirname + '/web/index.ejs', 'utf8');
+var sys_path_sep = path.sep;
+var sys_arg_length = process.argv.length;
+var web_base = __dirname + sys_path_sep + 'web';
+if (sys_arg_length > 2) {
+    web_base = process.argv[2];
+}
 
-var ret = ejs.render(index_ejs_str, {
-    'title': 'Hello EJS'
-});
+console.log(web_base);
 
-console.log(ret);
+// var sys_scope = {
+//     'version': '0.0.1'
+// };
+
+http.createServer(function (request, response) {
+
+    request.setEncoding("utf8");
+    var pathname = url.parse(request.url).pathname;
+
+    //TODO request params
+
+    var stat_result = fs.stat(web_base + pathname, function (err, stat) {
+        if (stat && stat.isFile()) {
+            console.log('文件存在');
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            var render_resp = ejs.renderFile(web_base + pathname, {
+                'title': 'Hello EJS'
+            }, function (err, result) {
+                console.log(err);
+                console.log(result);
+                response.end(result);
+            });
+            console.log(render_resp);
+        } else {
+            response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.end('404');
+            console.log('文件不存在或不是标准文件');
+        }
+    });
+    console.log(stat_result);
+
+    console.log(pathname);
+
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
